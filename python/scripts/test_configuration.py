effe7143d373adc1ce51aebdefe84fb6e77f96ef
Baptiste Busch <baptiste.busch@gmail.com>
2019-05-27 13:52:35 +0200

    Update test script

--- python/scripts/test_configuration.py
+++ python/scripts/test_configuration.py
@@ -11,29 +11,30 @@ from dynamic_obstacle_avoidance.visualization.animated_simulation import run_ani
 
 def main():
     N = 1
-    x_init = np.vstack((np.ones(N)*0,
-                        np.ones(N)*4))
+    x_init = np.vstack((np.ones(N)*1.22485,
+                        np.ones(N)*8))
 
     ### Create obstacle 
     obs = []
-    a = np.array([1, 1]) 
-    x0 = np.array([-1, 1])
-    sf = 1
-    obs.append(Obstacle(a=a, x0=x0, sf=sf))
+    a = np.array([1.7051, 1.22958]) 
+    x0 = np.array([-0.530183, 0.999465])
+    sf = 1 + 0.46373
+    th_r = 2 * np.pi - 2.31767
+    obs.append(Obstacle(a=a, x0=x0, sf=sf, th_r=th_r))
 
-    a = np.array([1, 1]) 
-    x0 = np.array([0.5, -0.5])
-    obs.append(Obstacle(a=a, x0=x0, sf=sf))
+    # a = np.array([1, 1]) 
+    # x0 = np.array([0.5, -0.5])
+    # obs.append(Obstacle(a=a, x0=x0, sf=sf))
 
-    a = np.array([1, 1]) 
-    x0 = np.array([1, 1.5])
-    obs.append(Obstacle(a=a, x0=x0, sf=sf))
+    # a = np.array([1, 1]) 
+    # x0 = np.array([1, 1.5])
+    # obs.append(Obstacle(a=a, x0=x0, sf=sf))
 
-    attractorPos = np.array([-1, -4])
+    attractorPos = np.array([4.90769, -8])
 
-    xRange = [-5, 5]
-    yRange = [-5, 5]
-    zRange = [-5, 5]
+    xRange = [-10, 10]
+    yRange = [-10, 10]
+    zRange = [-10, 10]
 
     animationName = 'test.mp4'
     run_animation(x_init, obs, xRange=xRange, yRange=yRange, dt=0.01, N_simuMax=1040, convergenceMargin=0.3, sleepPeriod=0.01, attractorPos=attractorPos, animationName=animationName, saveFigure=False)
